997b5bf340af8bbd774eeee13c64f868b3fb7846
Haikal Hussein <mhaikal.mhussein@gmail.com>
2010-09-28 15:44:43 +0100

    added files for b2b extension

---
+++ vendor/extensions/b2b/config/initializers/b2b.rb
@@ -0,0 +1,2 @@
+Spree::Config.set(:stylesheets => 'screen,styleaku,screenaku,simpletreeaku') 
+Spree::Config.set(:javascripts => 'autoresize.jquery,easySlider1.7,jquery,simpletreemenu,vanish' )
---
+++ vendor/extensions/b2b/public/javascripts/vanish.js
@@ -0,0 +1,12 @@
+
+
+				//<![CDATA[
+				function clearThis(obj, defText) {
+					//alert(obj.value);
+					if (obj.value == defText) obj.value = '';
+				}
+
+				function restoreThis(obj, defText) {
+					if (obj.value == '') obj.value = defText;
+				}
+				//]]>
